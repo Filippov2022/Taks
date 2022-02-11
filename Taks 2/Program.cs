b673ebb94a925df8c209c8c71222723f3aabb4c0
Александр <aleksandr.filippov98@mail.ru>
2022-02-11 17:07:41 +0300

    Наибольшее и меньшее число

---
+++ Taks 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int a = 6;
+int b = 10;
+int max = a;
+int min = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (a < min) max = a;
+if (b < min) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
