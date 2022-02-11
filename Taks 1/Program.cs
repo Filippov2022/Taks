e548052e3afee3b0d025f5b2254a83e564fae25a
Александр <aleksandr.filippov98@mail.ru>
2022-02-11 16:56:25 +0300

    По двум заданным числам проверять является ли первое квадратом второго

---
+++ Taks 1/Program.cs
@@ -0,0 +1,8 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+int a = 25;
+int b = 5;
+int c = b * b;
+if (c == a)
+Console.WriteLine("true");
+else 
+Console.WriteLine("false");
