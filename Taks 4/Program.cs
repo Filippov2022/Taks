b66a45bef20fdd20c883f50565c285c25ffe9d10
Александр <aleksandr.filippov98@mail.ru>
2022-02-11 20:17:08 +0300

    Максимальное из 3-х чисел

---
+++ Taks 4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти максимальное из трех чисел
+int a = 5;
+int b = 6;
+int c = 15;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
