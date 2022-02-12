99abef503865d36cc537b4cf80b3efcc4352c9d4
Александр <aleksandr.filippov98@mail.ru>
2022-02-12 17:29:28 +0300

    Максимальное и миниминимальное

---
+++ Taks 7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа от -N до N
+double max1= double.MaxValue;
+double min1 = double.MinValue;
+Console.WriteLine($"Числа от {min1} до {max1}");
+int max2 = int.MaxValue;
+int min2 = int.MinValue;
+Console.WriteLine($"Числа от {min2} до {max2}");
+decimal min3 = decimal.MinValue;
+decimal max3 = decimal.MaxValue;
+Console.WriteLine($"Числа от {min3} до {max3} ");
