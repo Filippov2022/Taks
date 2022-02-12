35965a3f024bf6ffa27bf3d36e2ab145fdff64fe
Александр <aleksandr.filippov98@mail.ru>
2022-02-12 11:49:56 +0300

    Чесло четное или не четное

---
+++ Taks 6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+int a;
+Console.Write("Введите число: ");
+a = Console.Read();
+if (a % 2 == 1)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+Console.ReadKey();
